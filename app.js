68c476533557bd07bc3bb73551c0c7f733d5b5a0
unknown <lucassocorrosilva@gmail.com>
2022-11-10 23:32:38 -0300

    Desafio em desenvolvimento

---
+++ app.js
@@ -0,0 +1,23 @@
+const url = 'https://api.adviceslip.com/advice';
+const result = document.getElementById('result');
+const btn = document.querySelector(".btn");
+const resultId = document.querySelector('.id')
+
+
+btn.addEventListener('click', () => {
+  fetchAdvice();
+})
+
+
+const fetchAdvice = async () => {
+ try {
+  const response  = await fetch(url);
+  const data = await response.json();
+  resultId.textContent = data.slip.id;
+  result.textContent = data.slip.advice;
+ } catch (error) {
+  console.log(error)
+ }
+}
+
+
